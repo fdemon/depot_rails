0147c7f8211546a56c04c729f392056fed3912fd
fdemon <heshanjun@gmail.com>
2013-01-30 21:27:11 +0800

    Valites

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+   test "Product price must be positive" do
+     product = Product.new(:title => "MY book title", :descript => "yyy", :image_url => "zzz.jpg")
+     product.price =-1
+     assert product.invalid?
+     assert_equal "must be greater than or equal to 0.01",product.errors[:price].join('; ')
+     
+     product.price = 0
+     assert product.invalid?
+     assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+     
+     product.price = 1
+     assert product.invalid?
+   end
+   
+   test "product id not valid without a unique title" do
+     product = Product.new(:title => products(:ruby).title, :descript => "yyy", :price => 1, :image_url => "fred.gif")
+     assert !product.save
+     assert_equal "has already been taken",product.errors[:title].join('; ')
+   end
 end
